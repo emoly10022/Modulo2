9545f6b2b768a253dfa0305f4d1e910abf47e51b
emoly1002 <110625232+emoly10022@users.noreply.github.com>
2022-11-21 20:20:55 -0300

    atualização da atividade

---
+++ 02_AUT_EST_ENTREGA/Semana 4/backend/app.js
@@ -0,0 +1,100 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = '../data/curriculoemelyatv1.db';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+/* Definição dos endpoints */
+/******** CRUD ************/
+app.use(express.json());
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/listaUsuario', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT * FROM usuario ORDER BY nome COLLATE NOCASE';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/insereUsuario', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    sql = "INSERT INTO usuario (nome, idade, e-mail, endereco, telefone) VALUES ('" + req.body.nome + "', '" + req.body.idade + "', " + req.body.email + ", '" + req.body.endereco + "', '" + req.body.telefone + "')";
+    console.log(sql);
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }	
+    });
+    res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+    db.close(); // Fecha o banco
+    res.end();
+});
+
+// Monta o formulário para o update (é o U do CRUD - Update)
+app.get('/atualizaUsuario', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    sql = "SELECT * FROM usuario WHERE cod_usuario="+ req.query.cod_usuario;
+    console.log(sql);
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/atualizaUsuario', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    sql = "UPDATE curriculo SET nome='" + req.body.nome + "', idade= '" + req.body.idade + "' , email='" + req.body.email + "', endereco= '" + req.body.endereco + "' , telefone= '"+req.body.telefone + "' WHERE cod_usuario='" + req.body.cod_usuario + "'";
+    console.log(sql);
+    var db = new sqlite3.Database(DBPATH);
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    res.write('<p>USUARIO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+    db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.get('/removeUsuario', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    sql = "DELETE FROM usuario WHERE cod_usuario='" + req.query.cod_usuario + "'";
+    console.log(sql);
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.write('<p>USUARIO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
